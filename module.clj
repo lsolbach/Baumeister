cc6e851e9ba6a9d5c9f3142a9f6b08f193865ab8
Ludger Solbach <soulman@soulspace.org>
2013-08-04 06:05:26 +0200

    bumped version to 0.4.5

--- module.clj
+++ module.clj
@@ -4,7 +4,7 @@
  :project-lead "Ludger Solbach"
  :description "Baumeister Build System"
  :type :consolefrontend
- :version "0.4.4"
+ :version "0.4.5"
  :author "Ludger Solbach"
  :provider "soulspace.org"
  :inception-year 2012
