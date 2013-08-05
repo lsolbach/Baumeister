c778675f56fbc949f3033cbea66f35b4edc5e144
Ludger Solbach <soulman@soulspace.org>
2013-08-05 02:31:48 +0200

    added copyright notice in source files

--- src/org/soulspace/build/baumeister/dependency/dependency_node.clj
+++ src/org/soulspace/build/baumeister/dependency/dependency_node.clj
@@ -1,3 +1,12 @@
+;
+;   Copyright (c) Ludger Solbach. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file license.txt at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+;
 (ns org.soulspace.build.baumeister.dependency.dependency-node
   (:require [clojure.zip :as zip])
   (:use [clojure.set :only [union]]
@@ -5,8 +14,7 @@
         [org.soulspace.build.baumeister.utils log]
         [org.soulspace.build.baumeister.config registry]
         [org.soulspace.build.baumeister.repository repositories]
-        [org.soulspace.build.baumeister.dependency dependency]
-        ))
+        [org.soulspace.build.baumeister.dependency dependency]))
 
 ;
 ; Transitive dependency functions 
