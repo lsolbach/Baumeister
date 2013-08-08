7ca2d6798cc686b015704558e1ca4c4a185de991
Ludger Solbach <soulman@soulspace.org>
2013-08-08 02:21:49 +0200

    initial import

---
+++ module.clj
@@ -0,0 +1,11 @@
+[
+ :module "BaumeisterJDependPlugin"
+ :project "org.soulspace.build"
+ :type :library
+ :version "0.5.1"
+ :description ""
+ :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
+ :dependencies [["org.clojure/clojure, 1.5.1"]
+                ["org.clojure/data.xml, 0.0.7"]
+                ["org.soulspace.clj/CljXmlLibrary, 0.3.0"]]
+ ]
