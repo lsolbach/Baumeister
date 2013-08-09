d2dd86b5bd26225aacdfe80884cb04efee60bb62
Ludger Solbach <soulman@soulspace.org>
2013-08-09 14:17:20 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -1,11 +1,12 @@
 [
  :module "BaumeisterPMDPlugin"
- :project "org.soulspace.build"
+ :project "org.soulspace.baumeister"
  :type :library
  :version "0.5.1"
  :description ""
  :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
  :dependencies [["org.clojure/clojure, 1.5.1"]
-                ["org.clojure/data.xml, 0.0.7"]
-                ["org.soulspace.clj/CljXmlLibrary, 0.3.0"]]
+                ["org.soulspace.baumeister" "Baumeister" "0.6.0"]
+                ["org.soulspace.clj/CljXmlLibrary, 0.3.0"]
+                ["net.sourceforge.pmd/pmd, 5.0.0"]]
  ]
