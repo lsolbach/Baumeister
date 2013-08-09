c0f605a15f7a042ece48743e998445d0d0f630a0
Ludger Solbach <soulman@soulspace.org>
2013-08-09 14:15:08 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -1,11 +1,13 @@
 [
  :module "BaumeisterJUnitPlugin"
- :project "org.soulspace.build"
+ :project "org.soulspace.baumeister"
  :type :library
  :version "0.5.1"
  :description ""
  :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
  :dependencies [["org.clojure/clojure, 1.5.1"]
-                ["org.clojure/data.xml, 0.0.7"]
-                ["org.soulspace.clj/CljXmlLibrary, 0.3.0"]]
+                ["org.soulspace.baumeister" "Baumeister" "0.6.0"]
+                ["org.soulspace.clj/CljXmlLibrary, 0.3.0"]
+                ["org.apache.ant/ant-junit, 1.8.3"] ; junit plugin
+                ]
  ]
