4395e8e51c12ee9ed1bfe50de88fac48f57e98be
Ludger Solbach <soulman@soulspace.org>
2013-08-09 13:47:36 +0200

    initial import

---
+++ module.clj
@@ -0,0 +1,10 @@
+[
+ :module "BaumeisterJavaPlugin"
+ :project "org.soulspace.baumeister"
+ :type :library
+ :version "0.5.1"
+ :description "Compiler plugin for Java"
+ :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
+ :dependencies [["org.clojure/clojure, 1.5.1"]
+                ["org.soulspace.baumeister" "Baumeister" "0.6.0"]]
+ ]
