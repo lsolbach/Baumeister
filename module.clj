dcc4521340af38fbb2c288d088597d72fedd81d3
Ludger Solbach <soulman@soulspace.org>
2013-08-09 13:52:47 +0200

    initial import

---
+++ module.clj
@@ -0,0 +1,10 @@
+[
+ :module "BaumeisterClojureTestPlugin"
+ :project "org.soulspace.baumeister"
+ :type :library
+ :version "0.5.1"
+ :description "Plugin for Clojure Test"
+ :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
+ :dependencies [["org.clojure/clojure, 1.5.1"]
+                ["org.soulspace.baumeister" "Baumeister" "0.6.0"]]
+ ]
