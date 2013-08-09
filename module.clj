6c50ea2d6067a48b06371fb7172b2292e81bfa33
Ludger Solbach <soulman@soulspace.org>
2013-08-09 14:18:03 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -1,11 +1,11 @@
 [
  :module "BaumeisterSitePlugin"
- :project "org.soulspace.build"
+ :project "org.soulspace.baumeister"
  :type :library
  :version "0.5.1"
  :description "The BaumeisterSitePlugin is a plugin for the Baumeister build system that creates web sites with information about modules and projects."
  :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
  :dependencies [["org.clojure/clojure, 1.5.1"]
-                ["org.clojure/data.xml, 0.0.7"]
+                ["org.soulspace.baumeister" "Baumeister" "0.6.0"]
                 ["org.soulspace.clj/CljXmlLibrary, 0.3.0"]]
  ]
