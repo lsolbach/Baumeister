c02058f62c904aef63e51858d60bb3ff8caf746e
Ludger Solbach <soulman@soulspace.org>
2013-08-09 13:16:29 +0200

    initial import

---
+++ module.clj
@@ -0,0 +1,13 @@
+[
+ :module "BaumeisterAspectJPlugin"
+ :project "org.soulspace.baumeister"
+ :type :library
+ :version "0.5.1"
+ :description ""
+ :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
+ :dependencies [["org.clojure/clojure, 1.5.1"]
+                ["org.soulspace.clj/CljXmlLibrary, 0.3.0"]
+                ["org.soulspace.baumeister" "Baumeister" "0.6.0"]
+                ["org.aspectj/aspectjrt, 1.6.11"]
+                ["org.aspectj/aspectjtools, 1.6.11"]]
+ ]
