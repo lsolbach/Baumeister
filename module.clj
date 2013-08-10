3c6d3001586910254b2c9404041766e972b25c65
Ludger Solbach <soulman@soulspace.org>
2013-08-10 07:22:22 +0200

    initial import, relocated from baumeister

---
+++ module.clj
@@ -0,0 +1,12 @@
+[
+ :module "BaumeisterMDDGeneratorPlugin"
+ :project "org.soulspace.baumeister"
+ :type :library
+ :version "0.5.1"
+ :description "MDD Generator plugin."
+ :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
+ :dependencies [["org.clojure/clojure, 1.5.1"]
+                ["org.soulspace.baumeister/Baumeister, 0.6.0"]
+                ["org.soulspace.clj/CljModelGenerator, 0.4.0"]
+                ]
+ ]
