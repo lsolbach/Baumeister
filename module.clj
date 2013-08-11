73e2f83f64a61ca041dfa279bf6e726c44f7324c
Ludger Solbach <soulman@soulspace.org>
2013-08-11 17:21:40 +0200

    updated module.clj

--- module.clj
+++ module.clj
@@ -6,5 +6,6 @@
  :description "Plugin for Clojure Test"
  :plugins ["global" "dependencies" "clojure" "clojuretest" "package"]
  :dependencies [["org.clojure/clojure, 1.5.1"]
-                ["org.soulspace.baumeister" "Baumeister" "0.6.0"]]
+                ["org.soulspace.baumeister/Baumeister, 0.6.0"]
+                ]
  ]
