87287bf1b6af8d31b2aaf8309c06898ef9e79595
Ludger Solbach <soulman@soulspace.org>
2014-11-19 20:44:37 +0100

    version bump to 0.6.3

--- Baumeister/config/module_defaults.clj
+++ Baumeister/config/module_defaults.clj
@@ -7,7 +7,7 @@
 ; If you want to override configuration parameters, use $(HOME)/.Baumeister/settings.clj or module.clj
 ;
 [
- :system-version "0.6.2"
+ :system-version "0.6.3"
  ;
  ; default log and message levels
  ;
--- Baumeister/module.clj
+++ Baumeister/module.clj
@@ -4,7 +4,7 @@
  :project-lead "Ludger Solbach"
  :description "Baumeister Build System"
  :type :consolefrontend
- :version "0.6.2"
+ :version "0.6.3"
  :author "Ludger Solbach"
  :provider "soulspace.org"
  :inception-year 2012
