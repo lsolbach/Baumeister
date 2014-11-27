46b71396525065c0a7063870d3cee9ef4ece0568
Ludger Solbach <soulman@soulspace.org>
2014-11-27 23:54:03 +0100

    added templates for AspectJ, Clojure and Java modules

---
+++ GenesisPlugin/templates/Clojure/module.clj
@@ -0,0 +1,11 @@
+[
+ :module "${new-module}"
+ :project "${new-project}"
+ :type :${new-type}
+ :version "0.1.0"
+ :description ""
+ :plugins [["org.soulspace.baumeister/DependencyPlugin"]
+           ["org.soulspace.baumeister/ClojurePlugin"]
+           ["org.soulspace.baumeister/PackagePlugin"]]
+ :dependencies [["org.clojure/clojure, 1.5.1"]]
+]
