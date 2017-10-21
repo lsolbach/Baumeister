e76149307ff2b80dbe1d7ff958fae4017f2886d4
Ludger Solbach <Ludger.Solbach@msg-systems.com>
2017-10-21 10:19:07 +0200

    updated build files

--- ClojureTestPlugin/module.clj
+++ ClojureTestPlugin/module.clj
@@ -2,8 +2,8 @@
  :module "ClojureTestPlugin"
  :project "org.soulspace.baumeister"
  :type :baumeister-plugin
- :version "0.6.5"
- :description "ClojureTest plugin for unit testing clojure code with the Baumeister build system."
+ :version "0.7.0"
+ :description "ClojureTest plugin for unit testing clojure code with the Baumeister build system"
  :plugins [["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
  :dependencies [["org.soulspace.baumeister/Baumeister" :dev]]
