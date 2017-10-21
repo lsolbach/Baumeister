0ab493cb798d26467fa1d4809c3655daa2fcde59
Ludger Solbach <Ludger.Solbach@msg-systems.com>
2017-10-21 09:53:42 +0200

    updated build files

--- AspectJDocPlugin/module.clj
+++ AspectJDocPlugin/module.clj
@@ -3,7 +3,7 @@
  :project "org.soulspace.baumeister"
  :type :baumeister-plugin
  :version "0.7.0"
- :description "AspectJDoc plugin for the Baumeister build system."
+ :description "AspectJDoc plugin for the Baumeister build system"
  :plugins [["org.soulspace.baumeister/ClojurePlugin, 0.6.5"]
            ["org.soulspace.baumeister/PackagePlugin, 0.6.7"]]
  :dependencies [["org.soulspace.baumeister/Baumeister, 0.6.9" :dev]
--- CheckstylePlugin/module.clj
+++ CheckstylePlugin/module.clj
@@ -2,7 +2,7 @@
  :module "CheckstylePlugin"
  :project "org.soulspace.baumeister"
  :type :baumeister-plugin
- :version "0.6.5"
+ :version "0.7.0"
  :description "CheckStyle statical code analysis plugin for the Baumeister build system."
  :plugins [["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
