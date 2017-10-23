cce4278cb51ef52661f29e7f731dd4c703a7db9c
Ludger Solbach <Ludger.Solbach@msg-systems.com>
2017-10-23 14:16:08 +0200

    added leiningen project files, updated build files

--- JavaPlugin/module.clj
+++ JavaPlugin/module.clj
@@ -2,8 +2,8 @@
  :module "JavaPlugin"
  :project "org.soulspace.baumeister"
  :type :baumeister-plugin
- :version "0.6.5"
- :description "Java compiler plugin for the Baumeister build system."
+ :version "0.7.0"
+ :description "Java compiler plugin for the Baumeister build system"
  :plugins [["org.soulspace.baumeister/ClojurePlugin"]
            ["org.soulspace.baumeister/PackagePlugin"]]
  :dependencies [["org.soulspace.baumeister/Baumeister" :dev]]
